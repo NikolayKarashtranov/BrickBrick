ca7c0e2632c52fde71efaac613da34bb403f42bf
unknown <frederick1190@gmail.com>
2019-02-09 17:25:18 +0200

    Snake case file names.

---
+++ brick_brick.rb
@@ -0,0 +1,30 @@
+require 'gosu'
+require 'fileutils'
+include Math
+require_relative 'src/values'
+require_relative 'src/visual_game_object'
+require_relative 'src/player'
+require_relative 'src/ball'
+require_relative 'src/brick'
+require_relative 'src/level'
+require_relative 'src/game_state'
+require_relative 'src/bonus'
+
+class BrickBrick < Gosu::Window
+  def initialize
+    FileUtils.mkdir_p 'save'
+    super SizeValues::SCREEN_WIDTH, SizeValues::SCREEN_HEIGHT
+    self.caption = self.class.to_s
+    @state = InitMenu.new(self)
+  end
+
+  def update
+    @state = @state.update
+  end
+
+  def draw
+    @state.draw
+  end
+end
+
+BrickBrick.new.show
